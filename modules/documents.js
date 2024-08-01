71c8dc48e28899194b50dfd726ffda9864390c20
spectrum-art <34438419+spectrum-art@users.noreply.github.com>
2024-07-31 19:11:32 -0700

    Added Template Literal for map iframe readability

--- modules/documents.js
+++ modules/documents.js
@@ -55,9 +55,19 @@ function setupDocumentsEventListeners(content) {
 
 function openCookieDeliveryMap() {
     console.log('Opening Cookie Delivery Map');
+    const mapUrl = `https://gta-5-map.com/?\
+        slideout=false&slideoutright=false&x=-120.1&y=80.5&zoom=3.4&embed=light\
+        &notes=3EWfhJLeGcb,3nf05rUzzTS,61hDtXO1IAV,6KSIzbU0JCX,78yKmWHpAxr,8qmes9jiqky,\
+        9LdfkbPEQUp,Akr3xVeFxPx,BzSCrsUcHX0,CAecif3MPtL,CxmrjyVaMdb,ErAwcUUL4Jv,FqeP7JRiEfO,\
+        Gg4LUImN5RM,GZAFGvIkhQl,HD2hOgesZEE,Hpc1RWCbYNJ,HxWPJdFD5zG,I02HCZZmolC,I6nFz53EbKo,\
+        JbMeXCoX67S,K0Gco51LKq8,KOFXc19AHzl,KuW1Kv0rFKa,tzvgY7VwaI`;
+
     const mapConfig = {
         title: 'Cookie Delivery Map',
-        content: `<iframe src="https://gta-5-map.com/?slideout=false&slideoutright=false&x=-120.10709928325205&y=80.48718362536638&zoom=3.4021903306057872&notes=3EWfhJLeGcb,3nf05rUzzTS,61hDtXO1IAV,6KSIzbU0JCX,78yKmWHpAxr,8qmes9jiqky,9LdfkbPEQUp,Akr3xVeFxPx,BzSCrsUcHX0,CAecif3MPtL,CxmrjyVaMdb,ErAwcUUL4Jv,FqeP7JRiEfO,Gg4LUImN5RM,GZAFGvIkhQl,HD2hOgesZEE,Hpc1RWCbYNJ,HxWPJdFD5zG,I02HCZZmolC,I6nFz53EbKo,JbMeXCoX67S,K0Gco51LKq8,KOFXc19AHzl,KuW1Kv0rFKa,tzvgY7VwaI&embed=light" style="border: none; width: 100%; height: 100%;" sandbox="allow-scripts allow-same-origin"></iframe>`,
+        content: `<iframe src="${mapUrl}" 
+            style="border: none; width: 100%; height: 100%;" 
+            sandbox="allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox">
+        </iframe>`,
         width: '90%',
         height: '90%',
         minWidth: '400px',
