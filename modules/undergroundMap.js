f1677b75e8599132250d4867467f06fa3ff119eb
spectrum-art <34438419+spectrum-art@users.noreply.github.com>
2024-08-04 19:19:31 -0700

    Update undergroundMap.js

--- modules/undergroundMap.js
+++ modules/undergroundMap.js
@@ -20,7 +20,7 @@ export function initializeUndergroundMap(container) {
 
     const map = L.map(container, {
         crs: L.CRS.Simple,
-        minZoom: -1,
+        minZoom: 0,
         maxZoom: 2,
         zoomControl: false
     });
