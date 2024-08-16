bc564f3954547e6db899ebd6c11716767aaaeb33
spectrum-art <34438419+spectrum-art@users.noreply.github.com>
2024-08-15 17:44:27 -0700

    Update particleBezier.js

--- modules/particleBezier.js
+++ modules/particleBezier.js
@@ -31,194 +31,99 @@ class Vector {
 class Particle {
   constructor(x, y) {
     this.pos = new Vector(x, y);
-    this.prevPos = new Vector(x, y);
-    this.vel = new Vector(Math.random() - 0.5, Math.random() - 0.5);
+    this.vel = new Vector(0, 0);
     this.acc = new Vector(0, 0);
+    this.color = "rgba(255, 255, 255, 0.5)";
   }
   
-  move(acc) {
-    this.prevPos.x = this.pos.x;
-    this.prevPos.y = this.pos.y;
-    if(acc) {
-      this.acc.addTo(acc);
-    }
+  update() {
     this.vel.addTo(this.acc);
     this.pos.addTo(this.vel);
-    if(this.vel.getLength() > config.particleSpeed) {
-      this.vel.setLength(config.particleSpeed);
-    }
     this.acc.x = 0;
     this.acc.y = 0;
-  }
-    
-  drawLine() {
-    ctx.beginPath();
-    ctx.moveTo(this.prevPos.x, this.prevPos.y);
-    ctx.lineTo(this.pos.x, this.pos.y);
-    ctx.stroke();  
+    this.vel.setLength(Math.min(this.vel.getLength(), 2));
   }
   
-  wrap() {
-    if(this.pos.x > w) {
-      this.prevPos.x = this.pos.x = 0;
-    } else if(this.pos.x < 0) {
-      this.prevPos.x = this.pos.x = w - 1;
-    }
-    if(this.pos.y > h) {
-      this.prevPos.y = this.pos.y = 0;
-    } else if(this.pos.y < 0) {
-      this.prevPos.y = this.pos.y = h - 1;
-    }
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.pos.x, this.pos.y, 2, 2);
   }
 }
 
-let canvas;
-let ctx;
-let field;
-let w, h;
-let size;
-let columns;
-let rows;
-let noiseZ;
-let particles;
-let config;
-let colorConfig;
-let buffer32;
+let canvas, ctx, w, h;
+let particles = [];
+let logoData;
 
-function setup(container, callback) {
-  console.log('Setup started');
-  size = 3;
-  noiseZ = 0;
+function setup(container) {
   canvas = document.createElement('canvas');
   canvas.style.width = '100%';
   canvas.style.height = '100%';
   container.appendChild(canvas);
-  ctx = canvas.getContext("2d", { willReadFrequently: true });
-  window.addEventListener("resize", () => reset(callback));  
-  config = {
-    zoom: 100,
-    noiseSpeed: 0.0071,
-    particleSpeed: 1.5,
-    fieldForce: 40,
-    randomForce: 10,
-  };
-
-  colorConfig = {
-    particleOpacity: 0.091,
-  };
-  reset(callback);
+  ctx = canvas.getContext("2d");
+  
+  window.addEventListener("resize", resize);
+  resize();
+  
+  loadLogo(() => {
+    initParticles();
+    draw();
+  });
 }
 
-function reset(callback) {
-  console.log('Reset called');
+function resize() {
   w = canvas.width = canvas.offsetWidth;
   h = canvas.height = canvas.offsetHeight;
-  columns = Math.floor(w / size) + 1;
-  rows = Math.floor(h / size) + 1;
-  initParticles();
-  initField();
-  drawText(() => {
-    drawBackground();
-    if(callback) callback();
-  });
+}
+
+function loadLogo(callback) {
+  const logo = new Image();
+  logo.onload = () => {
+    const tempCanvas = document.createElement('canvas');
+    const tempCtx = tempCanvas.getContext('2d');
+    tempCanvas.width = w;
+    tempCanvas.height = h;
+    
+    const scale = Math.min(w / logo.width, h / logo.height) * 0.8;
+    const scaledWidth = logo.width * scale;
+    const scaledHeight = logo.height * scale;
+    const x = (w - scaledWidth) / 2;
+    const y = (h - scaledHeight) / 2;
+    
+    tempCtx.drawImage(logo, x, y, scaledWidth, scaledHeight);
+    logoData = tempCtx.getImageData(0, 0, w, h).data;
+    callback();
+  };
+  logo.src = "../images/vixenLogoBlack.png";
 }
 
 function initParticles() {
-  console.log('Initializing particles');
   particles = [];
-  let numberOfParticles = w * h / 800;
-  for(let i = 0; i < numberOfParticles; i++) {
-    let particle = new Particle(
-      w/2 + Math.random()*400-200, 
-      h/2 + Math.random()*400-200);
-    particles.push(particle);
+  for (let i = 0; i < 5000; i++) {
+    particles.push(new Particle(Math.random() * w, Math.random() * h));
   }
-}
-
-function initField() {
-  console.log('Initializing field');
-  field = new Array(columns);
-  for(let x = 0; x < columns; x++) {
-    field[x] = new Array(columns);
-    for(let y = 0; y < rows; y++) {
-      field[x][y] = new Vector(0, 0);
-    }
-  }  
-}
-
-function calculateField() {
-  let x1;
-  let y1;
-  for(let x = 0; x < columns; x++) {
-    for(let y = 0; y < rows; y++) {
-      let color = buffer32[y*size * w + x*size];
-      if (color) {
-        x1 = (Math.random()-0.5) * config.randomForce;
-        y1 = (Math.random()-0.5) * config.randomForce;
-      } else {
-        x1 = noise3D(x/config.zoom, y/config.zoom, noiseZ) * config.fieldForce / 20;
-        y1 = noise3D(x/config.zoom + 40000, y/config.zoom + 40000, noiseZ) * config.fieldForce / 20;
-      }
-      field[x][y].x = x1;
-      field[x][y].y = y1;
-    }
-  }
-}
-
-function drawBackground() {
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, w, h);
-}
-
-function drawText(callback) {
-  console.log('Drawing text');
-  let logo = new Image();
-  logo.crossOrigin = "anonymous";
-  logo.src = "../images/vixenLogoBlack.png";
-  logo.onload = () => {
-    console.log('Image loaded');
-    let leftMargin = (w - logo.width) / 2;
-    let topMargin = (h - logo.height) / 2;
-    ctx.drawImage(logo, leftMargin, topMargin); 
-    let image = ctx.getImageData(0, 0, w, h);
-    buffer32 = new Uint32Array(image.data.buffer);
-    if(callback) callback();
-  };
-  logo.onerror = (e) => {
-    console.error('Error loading image:', e);
-    if(callback) callback();
-  };
-}
-
-function drawParticles() {
-  ctx.strokeStyle = `rgba(255, 0, 74, ${colorConfig.particleOpacity})`;
-  let x;
-  let y;
-  particles.forEach(p => {
-    x = p.pos.x / size;
-    y = p.pos.y / size;
-    let v;
-    if(x >= 0 && x < columns && y >= 0 && y < rows) {
-      v = field[Math.floor(x)][Math.floor(y)];
-    }
-    p.move(v);
-    p.wrap();
-    p.drawLine();
-  });
 }
 
 function draw() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+  ctx.fillRect(0, 0, w, h);
+  
+  particles.forEach(p => {
+    const index = (Math.floor(p.pos.y) * w + Math.floor(p.pos.x)) * 4;
+    if (logoData[index] > 0) {
+      p.acc = new Vector((Math.random() - 0.5) * 0.5, (Math.random() - 0.5) * 0.5);
+      p.color = "rgba(255, 0, 74, 0.8)";
+    } else {
+      const noise = noise3D(p.pos.x * 0.01, p.pos.y * 0.01, performance.now() * 0.0001);
+      p.acc = new Vector(Math.cos(noise * Math.PI * 2) * 0.1, Math.sin(noise * Math.PI * 2) * 0.1);
+      p.color = "rgba(255, 255, 255, 0.5)";
+    }
+    p.update();
+    p.draw();
+  });
+  
   requestAnimationFrame(draw);
-  calculateField();
-  noiseZ += config.noiseSpeed;
-  drawBackground();
-  drawParticles();
 }
 
 export function initialize(container) {
-  console.log('Initializing particleBezier');
-  setup(container, () => {
-    console.log('Setup complete, starting draw loop');
-    draw();
-  });
+  setup(container);
 }
