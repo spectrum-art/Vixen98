f538bb2eed56ee98b35c9bd752c0bd74069d2164
spectrum-art <34438419+spectrum-art@users.noreply.github.com>
2024-08-15 23:30:26 -0700

    Update placeholder.js

--- modules/placeholder.js
+++ modules/placeholder.js
@@ -189,7 +189,7 @@ function drawText(callback) {
 }
 
 function drawParticles() {
-  ctx.strokeStyle = `rgba(250, 70, 2, ${colorConfig.particleOpacity})`;
+  ctx.strokeStyle = `rgba(255, 60, 0, ${colorConfig.particleOpacity})`;
   ctx.lineWidth = 2;
   particles.forEach(p => {
     let x = p.pos.x / size;
